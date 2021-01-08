53a8dcf07434197ccbd11b43e689e8b9976fa260
Handsome121 <951791665@qq.com>
2021-01-08 16:19:14 +0800

    删除了一个文件夹

--- 1、刷题/1、leetcode刷题/strStr.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        L, N = len(needle), len(haystack)
-        for start in range(N - L + 1):
-            if haystack[start, start + L] == needle:
-                return start
-        return -1
-   
--- 1、刷题/1、leetcode刷题/数组中重复的元素.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findRepeatNumber(self, nums: List[int]) -> int:
-        repeatDict = {}
-        for num in nums:
-            if num not in repeateDict:
-                repeateDict[num] = 1
-            else:
-                return num
